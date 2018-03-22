776a1fd6533e6fd42978f0c385d1cb3664a74574
Eric Liao <rcliao01@gmail.com>
2018-03-22 07:47:24 -0700

    feat: add pomodoro channel example
    https://abronan.com/introduction-to-goroutines-and-go-channels/

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
+
+var (
+	currentTurn = 1
+	totalTurns  = 5
+	shortBreak  = 5 * time.Minute
+	longBreak   = 15 * time.Minute
+)
+
+func pomodoroTurn(chanPomodoro chan bool) {
+	say("Work work")
+	time.Sleep(time.Minute * 25)
+	say("Stop work")
+	chanPomodoro <- true
+}
+
+func pomodoroBreak(chanBreak chan bool, t time.Duration) {
+	say("Small break start")
+	time.Sleep(t)
+	say("Small break stop")
+	chanBreak <- true
+}
+
+func say(message string) {
+	exec.Command("say", message).Output()
+}
+
+func pomodoroService(chanPomodoro, chanBreak, chanLongBreak, chanDone chan bool) {
+	fmt.Println("Pomodoro started")
+	for {
+		select {
+		case <-chanPomodoro:
+			if currentTurn >= totalTurns {
+				go pomodoroBreak(chanLongBreak, longBreak)
+				currentTurn = 1
+			} else {
+				currentTurn++
+				go pomodoroBreak(chanBreak, shortBreak)
+			}
+		case <-chanBreak:
+			go pomodoroTurn(chanPomodoro)
+		case <-chanLongBreak:
+			input := askAnotherSession()
+			for input != "Y" && input != "N" {
+				input = askAnotherSession()
+			}
+			if input == "Y" {
+				go pomodoroTurn(chanPomodoro)
+			} else {
+				chanDone <- true
+			}
+		}
+	}
+}
+
+func askAnotherSession() string {
+	fmt.Println("Ready for another pomodoro session? (Y/N)")
+	var input string
+	fmt.Scanln(&input)
+	return input
+}
 
 func main() {
-	fmt.Println("Hello Chobits")
+	turn := make(chan bool)
+	smallBreak := make(chan bool)
+	longBreak := make(chan bool)
+	done := make(chan bool)
+
+	go pomodoroTurn(turn)
+	go pomodoroService(turn, smallBreak, longBreak, done)
+
+	<-done
 }
